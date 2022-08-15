9d5f2a5b9829c17b83ce58d203b40ff1b50f0dfb
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-08-15 15:59:36 +0000

    Created the layout of the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,73 @@
 import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+
+import useStyles from './styles';
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => {}}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+        )}
+        <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+        {!isMobile && 'Search...'}
+        <div>
+          {!isAuthenticated ? (
+            <Button color="inherit" onClick={() => {}}>
+              Login &nbsp; <AccountCircle />
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/profile/:id"
+              className={classes.LinkButton}
+              onClick={() => {}}
+            >
+              {!isMobile && <>My Movies &nbsp;</>}
+              <Avatar
+                style={{ width: 30, height: 30 }}
+                alt="Profile Picture"
+                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+              />
+            </Button>
+          )}
+        </div>
+        {isMobile && 'Search...'}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default NavBar;
