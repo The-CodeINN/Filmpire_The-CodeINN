1d8d876e032057db1910771beaef8c56754552d9
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-08-16 00:20:24 +0000

    API setup with redux toolkit

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 function Movies() {
+  const { data } = useGetMoviesQuery();
+  console.log(data);
+
   return <div>Movies</div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,10 +13,12 @@ const theme = createTheme({});
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
