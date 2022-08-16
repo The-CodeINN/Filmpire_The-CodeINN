a857d846593571e5d50ab0b73ecd81b95829c188
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-08-16 12:46:43 +0000

    Rendered the movies to the movie page

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -8,12 +8,42 @@ import {
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 function Movies() {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  return <div>Movies</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" ml="20px">
+        <Typography variant="h4">No movies that match that name.</Typography>
+        <br />
+        Please search for another movie.
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" alignItems="center" ml="20px">
+        <Typography variant="h4">Error: {error.message}</Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 }
 
 export default Movies;
